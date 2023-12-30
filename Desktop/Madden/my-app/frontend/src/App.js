b77ed4983a5800875015c556a604c8c4e2c1c081
Kyle <kylekibbe99@gmail.com>
2023-12-29 23:41:04 -0800

    added a clear function and some styling for loading

--- Desktop/Madden/my-app/frontend/src/App.js
+++ Desktop/Madden/my-app/frontend/src/App.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import FileUpload from './FileUpload';  // Ensure this path is correct
+import FileUpload from './FileUpload';  
 
 function App() {
   const [data, setData] = useState(null);
+
+  const clearData = () => {
+    setData(null);
+  };
 
   useEffect(() => {
     fetch('http://localhost:5000/data')
@@ -14,9 +18,16 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        {data ? <p>Name: {data.name}, Age: {data.age}</p> : <p>Loading...</p>}
-        <FileUpload />  {/* File Upload Component */}
+      <header className="App-header custom-header">
+        {data ? (
+          <div>
+            <p>Name: {data.name}, Age: {data.age}</p>
+            <button onClick={clearData}>Clear Data</button>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+        <FileUpload />
       </header>
     </div>
   );
